3b4202ee62507a08d54f4610b868e0a7a4998e45
elisebsm <elisesmebye@gmail.com>
2019-03-20 12:47:44 +0100

    krav om<

--- site/src/webapp/controllers/UserController.php
+++ site/src/webapp/controllers/UserController.php
@@ -46,27 +46,9 @@ class UserController extends Controller
 
         if($passwordConf == $password) {
             if ($this->hasCapLetters($password) && $this->hasNumbers($password) && $this->hasSpecialChars($password)) {
-                ##############################
-                $user = User::makeEmpty();
-                $user->setUsername($username);
-                $user->setPassword($password);
-
-                if ($request->post('email')) {
-                    $email = $request->post('email');
-                    $user->setEmail($email);
-                }
-                $user->save();
-                $this->app->flash('info', 'Thanks for creating a user. You may now log in.');
-                $this->app->redirect('/login');
-
-                ###############################
-                /*if (strpos($username, '<') !== false) {
+                if (strpos($username, '<') !== false) {
                     $this->app->flash('error', 'USERNAME CANNOT CONTAIN <');
                     $this->render('newUserForm.twig', []);
-                    #}
-                    # {
-                    #    $this->app->flash('info', 'USERNAME CANNOT CONTAIN <');
-                    #   $this->render('newUserForm.twig', []);
                 } else {
 
                     $user = User::makeEmpty();
@@ -80,7 +62,7 @@ class UserController extends Controller
                     $user->save();
                     $this->app->flash('info', 'Thanks for creating a user. You may now log in.');
                     $this->app->redirect('/login');
-                }*/
+                }
 
             } else {
                 $this->app->flash('error', 'THE PASSWORD DOES NOT CONTAIN ALL THE REQUIREMENTS ');
