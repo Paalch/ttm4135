cf0f5627214ffc48d508f48e4b4faeb0874dd515
Paalch <pciverse@stud.ntnu.no>
2019-03-23 15:09:18 +0100

    fixed create user

--- site/src/webapp/controllers/UserController.php
+++ site/src/webapp/controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
         parent::__construct();
     }
 
-    function index()     
+    function index()
     {
         if (Auth::guest()) {
             $this->render('newUserForm.twig', []);
@@ -24,68 +24,65 @@ class UserController extends Controller
     }
 
     // Does string contain letters?
-    function hasCapLetters( $string ) {
+    function hasCapLetters($string)
+    {
         return (!ctype_upper($string) && !ctype_lower($string));
     }
+
     // Does string contain numbers?
-    function hasNumbers( $string ) {
-        return preg_match( '/\d/', $string );
+    function hasNumbers($string)
+    {
+        return preg_match('/\d/', $string);
     }
+
     // Does string contain special characters?
-    function hasSpecialChars( $string ) {
+    function hasSpecialChars($string)
+    {
         return preg_match('/[^a-zA-Z\d]/', $string);
     }
 
-    function create()		  
+    function create()
     {
         $request = $this->app->request;
         $username = $request->post('username');
 
         $password = $request->post('password');
-
         $passwordConf = $request->post('passwordConf');
 
-        if($passwordConf == $password) {
-            if ($this->hasCapLetters($password) && $this->hasNumbers($password) && $this->hasSpecialChars($password)) {
-                if (strpos($username, '<') !== false) {
-                    $this->app->flash('error', 'USERNAME CANNOT CONTAIN <');
-                    $this->render('newUserForm.twig', []);
-                } else {
 
-                    $user = User::makeEmpty();
+        if (!$passwordConf === $password) {
+            $this->app->flashNow('error', 'The password does not meet all the requirements');
+            $this->render('newUserForm.twig', []);
+        } else if (!$this->hasCapLetters($password) || !$this->hasNumbers($password) || !$this->hasSpecialChars($password)) {
+            $this->app->flashNow('error', 'The password does not meet all the requirements');
+            $this->render('newUserForm.twig', []);
+        } else if (preg_match('/[^a-z0-9 _]+$/i', $username)) {
+            $this->app->flashNow('error', 'Username may only contain ASCII letters and digits, 
+            with hyphens, underscores and spaces as internal separators');
+            $this->render('newUserForm.twig', []);
+        } else if ((bool)User::findByUser($username)) {
+            $this->app->flashNow('error', 'The given username is already in use');
+            $this->render('newUserForm.twig', []);
+        } else {
+            $user = User::makeEmpty();
+            $user->setUsername($username);
+            $user->setPassword($password);
 
-                    $user->setUsername($username);
-                    //check if the username already exist
-                    $user = User::findByUser($username);
-                    if ($user !== 0) {
-                        $this->app->flash('error', 'THE USERNAME IS ALREADY TAKEN');
-                    }
-
-                    $user->setPassword($password);
-
-                    if ($request->post('email')) {
-                        $email = $request->post('email');
-                        $user->setEmail($email);
-                    }
-                    $user->save();
-                    $this->app->flash('info', 'Thanks for creating a user. You may now log in.');
-                    $this->app->redirect('/login');
-                }
-
-
-            } else {
-                $this->app->flash('error', 'THE PASSWORD DOES NOT MEET ALL THE REQUIREMENTS ');
-                $this->render('newUserForm.twig', []);
+            if ($request->post('email')) {
+                $email = $request->post('email');
+                $user->setEmail($email);
             }
 
+            $user->save();
+            $this->app->flash('info', 'Thanks for creating a user. You may now log in.');
+            $this->app->redirect('/login');
         }
 
     }
 
     function delete($tuserid)
     {
-        if(Auth::userAccess($tuserid))
-        {
+        if (Auth::userAccess($tuserid)) {
             $user = User::findById($tuserid);
             $user->delete();
             $this->app->flash('info', 'User ' . $user->getUsername() . '  with id ' . $tuserid . ' has been deleted.');
@@ -99,41 +96,39 @@ class UserController extends Controller
 
     function deleteMultiple()
     {
-      if(Auth::isAdmin()){
-          $request = $this->app->request;
-          $userlist = $request->post('userlist'); 
-          $deleted = [];
+        if (Auth::isAdmin()) {
+            $request = $this->app->request;
+            $userlist = $request->post('userlist');
+            $deleted = [];
 
-          if($userlist == NULL){
-              $this->app->flash('info','No user to be deleted.');
-          } else {
-               foreach( $userlist as $duserid)
-               {
+            if ($userlist == NULL) {
+                $this->app->flash('info', 'No user to be deleted.');
+            } else {
+                foreach ($userlist as $duserid) {
                     $user = User::findById($duserid);
-                    if(  $user->delete() == 1) { //1 row affect by delete, as expect..
-                      $deleted[] = $user->getId();
+                    if ($user->delete() == 1) { //1 row affect by delete, as expect..
+                        $deleted[] = $user->getId();
                     }
-               }
-               $this->app->flash('info', 'Users with IDs  ' . implode(',',$deleted) . ' have been deleted.');
-          }
+                }
+                $this->app->flash('info', 'Users with IDs  ' . implode(',', $deleted) . ' have been deleted.');
+            }
 
-          $this->app->redirect('/admin');
-      } else {
-          $username = Auth::user()->getUserName();
-          $this->app->flash('info', 'You do not have access this resource. You are logged in as ' . $username);
-          $this->app->redirect('/');
-      }
+            $this->app->redirect('/admin');
+        } else {
+            $username = Auth::user()->getUserName();
+            $this->app->flash('info', 'You do not have access this resource. You are logged in as ' . $username);
+            $this->app->redirect('/');
+        }
     }
 
 
-    function show($tuserid)   
+    function show($tuserid)
     {
-        if(Auth::userAccess($tuserid))
-        {
-          $user = User::findById($tuserid);
-          $this->render('showuser.twig', [
-            'user' => $user
-          ]);
+        if (Auth::userAccess($tuserid)) {
+            $user = User::findById($tuserid);
+            $this->render('showuser.twig', [
+                'user' => $user
+            ]);
         } else {
             $username = Auth::user()->getUserName();
             $this->app->flash('info', 'You do not have access this resource. You are logged in as ' . $username);
@@ -142,7 +137,7 @@ class UserController extends Controller
     }
 
     function newuser()
-    { 
+    {
 
         $user = User::makeEmpty();
 
@@ -156,7 +151,7 @@ class UserController extends Controller
             $email = $request->post('email');
 
             $isAdmin = ($request->post('isAdmin') != null);
-            
+
 
             $user->setUsername($username);
             $user->setPassword($password);
@@ -176,12 +171,12 @@ class UserController extends Controller
         }
     }
 
-    function edit($tuserid)    
-    { 
+    function edit($tuserid)
+    {
 
         $user = User::findById($tuserid);
 
-        if (! $user) {
+        if (!$user) {
             throw new \Exception("Unable to fetch logged in user's object from db.");
         } elseif (Auth::userAccess($tuserid)) {
 
@@ -193,7 +188,7 @@ class UserController extends Controller
             $email = $request->post('email');
 
             $isAdmin = ($request->post('isAdmin') != null);
-            
+
 
             $user->setUsername($username);
             $user->setPassword($password);
